4360896a8c623e98f6a032b508eeca5271215323
Thibaud Marin <thibaud.marin1@gmail.com>
2022-05-26 15:53:12 +0200

    commit de fin de journée jeudi, presque fini page profil

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,20 @@
+// Fonction menu déroulant
 menuB.addEventListener('click', displayList);
 
 function displayList() {
     menuList.classList.toggle('active')
+}
+
+// fonction confirmation du mot de passe 
+
+signInButton.addEventListener('click', validate)
+
+function validate() {
+    var a = document.getElementById("password").value;
+    var b = document.getElementById("Passwordconfirm").value;
+    if (a!=b) alert("Les mots de passe ne correspondent pas.");
+    else {
+    alert("Les mots de passe correspondent.");
+    document.f.submit();
+    }
 }
